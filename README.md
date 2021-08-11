b1a4946b43561123dd907d0a614e3c5b9c503b81
Uri Herrera <hello@uriherrera.com>
2021-08-10 20:56:50 -0500

    create pkg

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# index
-Mirror of https://invent.kde.org/maui/index-fm
+# Index [![Build Status](https://travis-ci.org/Nitrux/index.svg?branch=main)](https://travis-ci.org/Nitrux/index)
+
+Multi-platform file manager.
+
+# Issues
+If you find problems with the contents of this repository please create an issue.
+
+©2021 Nitrux Latinoamericana S.C.
---
+++ travis-build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -x
+
+apt -qq update
+apt -qq -yy install equivs curl git wget gnupg2
+
+### FIXME - the container mauikit/ubuntu-18.04-amd64 does have the neon repo but for some idiotic reason it isn't working here
+
+wget -qO /etc/apt/sources.list.d/neon-user-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.neon.user
+
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys \
+	55751E5D > /dev/null
+
+curl -L https://packagecloud.io/nitrux/repo/gpgkey | apt-key add -;
+
+wget -qO /etc/apt/sources.list.d/nitrux-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.nitrux
+
+apt -qq update
+
+### Install Dependencies
+
+DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends devscripts debhelper gettext lintian build-essential automake autotools-dev cmake extra-cmake-modules appstream qml-module-qtquick-controls2 qml-module-qtquick-shapes qml-module-qtgraphicaleffects mauikit-dev
+
+mk-build-deps -i -t "apt-get --yes" -r
+
+### Clone repo.
+
+git clone --single-branch --branch v2.0 https://invent.kde.org/maui/index-fm.git
+
+mv index-fm/* .
+
+rm -rf index-fm .vscode android_files macos_files windows_files ios_files screenshots LICENSES README.md
+
+### Build Deb
+
+mkdir source
+mv ./* source/ # Hack for debuild
+cd source
+debuild -b -uc -us
