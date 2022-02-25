a33427a9ace9e54f36811176f53d01809307740e
UriHerrera <hello@uriherrera.com>
2022-02-24 18:42:36 -0600

    update pkg

--- travis-build.sh
+++ travis-build.sh
@@ -99,7 +99,7 @@ checkinstall -D -y \
 	--install=no \
 	--fstrans=yes \
 	--pkgname=index-git \
-	--pkgversion=2.1.1+git+1 \
+	--pkgversion=2.1.1+git+2 \
 	--pkgarch=amd64 \
 	--pkgrelease="1" \
 	--pkglicense=LGPL-3 \
