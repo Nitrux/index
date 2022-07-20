106f696ff751b2ed5b4433f35f563e24b1c0531c
Uri Herrera <hello@uriherrera.com>
2022-07-19 23:02:06 -0500

    update stuff

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -1,69 +1,34 @@
-#!/bin/bash
+#! /bin/bash
 
 set -x
 
-### Install Build Tools #1
-
-DEBIAN_FRONTEND=noninteractive apt -qq update
-DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends \
-	appstream \
-	automake \
-	autotools-dev \
-	build-essential \
-	checkinstall \
-	cmake \
-	curl \
-	devscripts \
-	equivs \
-	extra-cmake-modules \
-	gettext \
-	git \
-	gnupg2 \
-	lintian \
-	wget
-
-### Add Neon Sources
+### Update sources
 
 wget -qO /etc/apt/sources.list.d/neon-user-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.neon.user
+
+wget -qO /etc/apt/sources.list.d/nitrux-main-compat-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.nitrux
+
+wget -qO /etc/apt/sources.list.d/nitrux-testing-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.nitrux.testing
 
 DEBIAN_FRONTEND=noninteractive apt-key adv --keyserver keyserver.ubuntu.com --recv-keys \
 	55751E5D > /dev/null
 
+curl -L https://packagecloud.io/nitrux/repo/gpgkey | apt-key add -;
+curl -L https://packagecloud.io/nitrux/compat/gpgkey | apt-key add -;
 curl -L https://packagecloud.io/nitrux/testing/gpgkey | apt-key add -;
-
-wget -qO /etc/apt/sources.list.d/nitrux-testing-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.nitrux.testing
 
 DEBIAN_FRONTEND=noninteractive apt -qq update
 
 ### Install Package Build Dependencies #2
-### Index needs ECM > 5.70
 
 DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends \
-	libkf5archive-dev \
-	libkf5config-dev \
-	libkf5coreaddons-dev \
-	libkf5i18n-dev \
-	libkf5kio-dev \
-	libkf5notifications-dev \
-	libkf5service-dev \
-	libkf5syntaxhighlighting-dev \
-	libqt5svg5-dev \
-	libwayland-dev \
-	mauikit-filebrowsing-git \
 	mauikit-git \
-	qtbase5-dev \
-	qtdeclarative5-dev \
-	qtquickcontrols2-5-dev \
-	qtwayland5 \
-	qtwayland5-dev-tools \
-	qtwayland5-private-dev
+	mauikit-accounts-git \
+	mauikit-filebrowsing-git
 
-DEBIAN_FRONTEND=noninteractive apt -qq -yy install --only-upgrade \
-	extra-cmake-modules
+### Download Source
 
-### Clone repo.
-
-git clone --single-branch --branch master https://invent.kde.org/maui/index-fm.git
+git clone --depth 1 --branch $INDEX_BRANCH https://invent.kde.org/maui/index-fm.git
 
 rm -rf index-fm/{.vscode,android_files,macos_files,windows_files,ios_files,screenshots,LICENSES,README.md}
 
@@ -73,7 +38,7 @@ sed -i '27d' index-fm/src/main.qml
 
 ### Compile Source
 
-mkdir -p index-fm/build && cd index-fm/build
+mkdir -p build && cd build
 
 cmake \
 	-DCMAKE_INSTALL_PREFIX=/usr \
@@ -86,12 +51,11 @@ cmake \
 	-DCMAKE_FIND_PACKAGE_NO_PACKAGE_REGISTRY=ON \
 	-DCMAKE_INSTALL_RUNSTATEDIR=/run "-GUnix Makefiles" \
 	-DCMAKE_VERBOSE_MAKEFILE=ON \
-	-DCMAKE_INSTALL_LIBDIR=lib/x86_64-linux-gnu ..
+	-DCMAKE_INSTALL_LIBDIR=lib/x86_64-linux-gnu ../index-fm/
 
-make
+make -j$(nproc)
 
 ### Run checkinstall and Build Debian Package
-### DO NOT USE debuild, screw it
 
 >> description-pak printf "%s\n" \
 	'MauiKit File Manager.' \
@@ -107,13 +71,13 @@ checkinstall -D -y \
 	--install=no \
 	--fstrans=yes \
 	--pkgname=index-git \
-	--pkgversion=2.1.2+git+4 \
+	--pkgversion=$PACKAGE_VERSION \
 	--pkgarch=amd64 \
 	--pkgrelease="1" \
 	--pkglicense=LGPL-3 \
 	--pkggroup=utils \
 	--pkgsource=index-fm \
-	--pakdir=../.. \
+	--pakdir=. \
 	--maintainer=uri_herrera@nxos.org \
 	--provides=index \
 	--requires="libc6,libkf5archive5,libkf5configcore5,libkf5coreaddons5,libkf5i18n5,libkf5kiocore5,libkf5service5,mauikit-git \(\>= 2.1.2+git+1\),mauikit-filebrowsing-git \(\>= 2.1.2+git+1\),libqt5core5a,libqt5gui5,libqt5qml5,libqt5widgets5,libstdc++6,qml-module-qt-labs-platform" \
