30d6901697b6f2edef2bed8e9c93ff2057ae9716
UriHerrera <hello@uriherrera.com>
2025-06-15 20:17:06 -0600

    update ci scripts

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -1,38 +1,50 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
-set -x
+#############################################################################################################################################################################
+#   The license used for this file and its contents is: BSD-3-Clause                                                                                                        #
+#                                                                                                                                                                           #
+#   Copyright <2025> <Uri Herrera <uri_herrera@nxos.org>>                                                                                                                   #
+#                                                                                                                                                                           #
+#   Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:                          #
+#                                                                                                                                                                           #
+#    1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.                                        #
+#                                                                                                                                                                           #
+#    2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer                                      #
+#       in the documentation and/or other materials provided with the distribution.                                                                                         #
+#                                                                                                                                                                           #
+#    3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software                    #
+#       without specific prior written permission.                                                                                                                          #
+#                                                                                                                                                                           #
+#    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,                      #
+#    THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS                  #
+#    BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE                 #
+#    GOODS OR SERVICES; LOSS OF USE, DATA,   OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,                      #
+#    STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.   #
+#############################################################################################################################################################################
 
-### Update sources
 
-mkdir -p /etc/apt/keyrings
+# -- Exit on errors.
 
-curl -fsSL https://packagecloud.io/nitrux/mauikit/gpgkey | gpg --dearmor -o /etc/apt/keyrings/nitrux_mauikit-archive-keyring.gpg
+set -e
 
-cat <<EOF > /etc/apt/sources.list.d/nitrux-mauikit.list
-deb [signed-by=/etc/apt/keyrings/nitrux_mauikit-archive-keyring.gpg] https://packagecloud.io/nitrux/mauikit/debian/ trixie main
-EOF
 
-apt -q update
+# -- Download Source
 
-### Install Package Build Dependencies #2
-
-apt -qq -yy install --no-install-recommends \
-	mauikit-git \
-	mauikit-filebrowsing-git
-
-### Download Source
-
-git clone --depth 1 --branch $INDEX_BRANCH https://invent.kde.org/maui/index-fm.git
+git clone --depth 1 --branch "$INDEX_BRANCH" https://invent.kde.org/maui/index-fm.git
 
 rm -rf index-fm/{.vscode,android_files,macos_files,windows_files,ios_files,screenshots,LICENSES,README.md}
 
-#### Remove accentcolor property
+
+# -- Remove accentcolor property
 
 sed -i '27d' index-fm/src/main.qml
 
-### Compile Source
+
+# -- Compile Source
 
 mkdir -p build && cd build
+
+HOST_MULTIARCH=$(dpkg-architecture -qDEB_HOST_MULTIARCH)
 
 cmake \
 	-DCMAKE_INSTALL_PREFIX=/usr \
@@ -45,13 +57,15 @@ cmake \
 	-DCMAKE_FIND_PACKAGE_NO_PACKAGE_REGISTRY=ON \
 	-DCMAKE_INSTALL_RUNSTATEDIR=/run "-GUnix Makefiles" \
 	-DCMAKE_VERBOSE_MAKEFILE=ON \
-	-DCMAKE_INSTALL_LIBDIR=/usr/lib/x86_64-linux-gnu ../index-fm/
+	-DCMAKE_INSTALL_LIBDIR="/usr/lib/${HOST_MULTIARCH}" \
+	../index-fm/
 
-make -j$(nproc)
+make -j"$(nproc)"
 
 make install
 
-### Run checkinstall and Build Debian Package
+
+# -- Run checkinstall and Build Debian Package
 
 >> description-pak printf "%s\n" \
 	'MauiKit File Manager.' \
@@ -66,9 +80,9 @@ make install
 checkinstall -D -y \
 	--install=no \
 	--fstrans=yes \
-	--pkgname=index-git \
-	--pkgversion=$PACKAGE_VERSION \
-	--pkgarch=amd64 \
+	--pkgname=index \
+	--pkgversion="$PACKAGE_VERSION" \
+	--pkgarch="$(dpkg --print-architecture)" \
 	--pkgrelease="1" \
 	--pkglicense=LGPL-3 \
 	--pkggroup=utils \
@@ -76,7 +90,7 @@ checkinstall -D -y \
 	--pakdir=. \
 	--maintainer=uri_herrera@nxos.org \
 	--provides=index \
-	--requires="kio-extras,libkf6kiofilewidgets6,libkf6kiogui6,libqt6multimedia6,libqt6multimediawidgets6,libqt6spatialaudio6,mauikit-archiver-git \(\>= 4.0.1\),mauikit-filebrowsing-git \(\>= 4.0.1\),mauikit-git \(\>= 4.0.1\),qml6-module-qtcore,qml6-module-qtmultimedia,qml6-module-qtquick-effects,qml6-module-qtquick3d-spatialaudio" \
+	--requires="kio-extras,libkf6kiofilewidgets6,libkf6kiogui6,libqt6multimedia6,libqt6multimediawidgets6,libqt6spatialaudio6,mauikit-archiver \(\>= 4.0.2\),mauikit-filebrowsing \(\>= 4.0.2\),mauikit \(\>= 4.0.2\),qml6-module-qtcore,qml6-module-qtmultimedia,qml6-module-qtquick-effects,qml6-module-qtquick3d-spatialaudio" \
 	--nodoc \
 	--strip=no \
 	--stripso=yes \
